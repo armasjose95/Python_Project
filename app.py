7bd31472039fd8b274208637538d38481bdb7f39
Jose Armas <armasjose95@gmail.com>
2022-12-27 17:41:38 -0600

    booleans

--- app.py
+++ app.py
@@ -120,9 +120,16 @@ print(max(5, -9, 27, 89))  # = 89
 # Practice problems
 # A.  The sum of the first five positive integers
 print(1+2+3+4+5)  # = 15
+# or
+a = (1, 2, 3, 4, 5)
+sum(a)
 
 # B. The average age of Sara (age 23), Mark (age 19), and Fatima (age 31)
-print((23+19+31) / 3)
+print((23+19+31) / 3)  # = 24.33
+# or
+age = (23, 19, 31)
+average_age = sum(age)/len(age)
+print(average_age)
 
 # C.  The number of times 73 goes into 403
 print(403//73)  # = 5
@@ -134,7 +141,82 @@ print(403 % 73)  # = 38
 print(2**10)  # = 1024
 
 # F. The absolute value of the difference between Sara’s height (54 inches) and Mark’s height (57 inches)
-print(abs(54-57))
+print(abs(54-57))  # = 3
 
 # G. The lowest price among the following prices: $34.99, $29.95, and $31.50
 print(min(34.99, 29.95, 31.50))  # = 29.95
+
+
+# Boolean Expressions-expressions that evaluate to one of two Boolean values: True or False.
+
+print(2 < 3)  # = True
+print(3 < 2)  # = False
+# algebraic expressions on either side of a comparison operators are evaluated before the comparison is made.
+print(5 - 1 > 2 + 1)  # = True
+
+
+# ==
+# In order to check equality between values, the comparison operator == is used.
+3 == 3  # = True
+3+5 == 4+4  # = True
+3 == 5-3  # = False
+
+# There are a few other logical comparison operators:
+# <= operator to test whether the expression on the left (3) is less than or equal to the expression of the right (4)
+3 <= 4  # = True
+# test whether the operand on the left is greater than or equal to the operand on the right
+3 >= 4  # = False
+# uses the != (not equal) operator to test whether the expressions on the left and right evaluate to different values.
+3 != 4  # = True
+
+
+# Practice problems
+# (a) The sum of 2 and 2 is less than 4.
+2+2 < 4  # = False
+# (b) The value of 7 // 3 is equal to 1 + 1.
+7 // 3 == 1+1  # = True 2==2
+# (c) The sum of 3 squared and 4 squared is equal to 25.
+(3**2) + (4**2) == 25  # = True
+# (d) The sum of 2, 4, and 6 is greater than 12.
+2+4+6 > 12  # = False
+# or
+x = (2, 4, 6)
+sum(x) > 12
+# (e) 1387 is divisible by 19.
+1387 % 19 == 0  # = True 3187//19 divides into a whole int.
+# (f) 31 is even. (Hint: what does the remainder when you divide by 2 tell you?)
+31 % 2 == 0  # = False 31%2 remainder is 1 so false
+# (g) The lowest price among $34.99, $29.95, and $31.50 is less than $30.00.
+min(34.99, 29.95, 31.50) < 30  # = True 29.95<30
+
+#Boolean expressions can be combined together using Boolean operators and , or , and not to form larger Boolean expressions.#
+# AND Operator
+# The and operator applied to two Boolean expressions will evaluate to True if both expressions evaluate to True;
+# if either expression evaluates to False, then it will evaluate to False:
+
+2 < 3 and 4 > 5  # = False, both not true
+2 < 3 and 1 < 4  # = True, both are true
+
+# OR Operator
+# The or operator applied to two Boolean expressions evaluates to False only when both expressions are false.
+# If either one is true or if both are true, then it evaluates to True.
+
+3 < 4 or 4 < 3  # = True, one of these are true
+3 < 2 or 2 < 1  # = False, both are false
+
+# NOT Operator
+# The not operator is a unary Boolean operator,
+# which means that it is applied to a single Boolean expression (as opposed to the binary Boolean operators and and or).
+# It evaluates to False if the expression is true or to True if the expression is false.
+
+not (3 < 4)  # = False
+not (3 > 4)  # = True
+not (4 < 3)  # = True
+
+# assigning names to values is called Variables
+# statement x=3 is called an assignment statement.
+# The general format of an assignment statement is:
+# <variable> = <expression>
+# variable x can be thought of as a name that enables us to retrieve value 3 later on.
+x = 3
+x  # or print(x)
