98519fdb645519f895f726188ee32fbec5962ee9
Jose Armas <armasjose95@gmail.com>
2023-06-12 09:02:09 -0500

    trying to sort a nested list using the bubble sort method

--- app.py
+++ app.py
@@ -3933,3 +3933,30 @@ nested2(5)
 # 0 1 2
 # 0 1 2 3
 # 0 1 2 3 4
+
+
+# 5.8
+
+# One way to sort a list of n different numbers in increasing order is to execute n − 1 passes
+# over the numbers in the list. Each pass compares all adjacent numbers in the list and swaps
+# them if they are out of order. At the end of the first pass, the largest item will be the last in
+# the list (at index n−1). Therefore, the second pass can stop before reaching the last element,
+# as it is already in the right position; the second pass will place the second largest item in the
+# next to last position. In general, pass i will compare pairs at indexes 0 and 1, 1 and 2, 2 and
+# 3, . . . , and i − 1 and i; at the end of the pass, the ith largest item will be at index n − i.
+# Therefore, after pass n − 1, the list will be in increasing order.
+# Write a function bubbleSort() that takes a list of numbers as input and sorts the list using this approach.
+
+
+#lst = [3, 1, 7, 4, 9, 2, 5]
+
+def bubbleSort(lst):
+    lst = [3, 1, 7, 4, 9, 2, 5]
+    for j in range(0, lst-1):
+        for i in range(lst):
+            if lst[i] - lst[i - 1] == diff:
+                return False
+        return True
+
+
+lst
