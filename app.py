bcb605e32c5586889f127f4887fc1ae31422726b
Jose Armas <armasjose95@gmail.com>
2023-08-29 23:56:00 -0500

    created a dictionary and moidified it by popping a key

--- app.py
+++ app.py
@@ -6083,3 +6083,73 @@ else:
 print("Yes") if 5 > 2 else print("No")
 
 # Yes
+
+
+# Use the get method to print the value of the "model" key of the car dictionary.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+print(car.get("model"))
+# or
+print(car['model'])
+
+
+# Change the "year" value from 1964 to 2020.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+car["year"] = 2020
+print(car)
+
+# = {'brand': 'Ford', 'model': 'Mustang', 'year': 2020}
+
+
+# Add the key/value pair "color" : "red" to the car dictionary.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+car['color'] = 'red'
+print(car)
+
+
+#{'brand': 'Ford', 'model': 'Mustang', 'year': 1964, 'color': 'red'}
+
+
+# Use the pop method to remove "model" from the car dictionary.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+car.pop("model")
+print(car)
+
+
+# = {'brand': 'Ford', 'year': 1964}
+
+
+# Use the clear method to empty the car dictionary.
+
+car = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964
+}
+
+car.clear()
+print(car)
+# = {}
