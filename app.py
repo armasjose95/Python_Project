3faac409e398dd6086288f03a61f832020c1babc
Jose Armas <armasjose95@gmail.com>
2023-09-12 16:29:12 -0500

    creating if,elif statments to calculate math statements

--- app.py
+++ app.py
@@ -6692,3 +6692,42 @@ multable(10)
 # 16
 # 18
 # 20
+
+
+# Write a program to display only those numbers from a list that satisfy the following conditions
+# The number must be divisible by five
+# If the number is greater than 150, then skip it and move to the next number
+# If the number is greater than 500, then stop the loop
+
+def lst(numbers):
+    for i in numbers:
+        if i > 500:
+            break
+        elif i > 150:
+            continue
+        elif i % 5 == 0:
+            print(i)
+
+
+lst([12, 75, 150, 180, 145, 525, 50])
+
+
+# 75
+# 150
+# 145
+
+
+# Write a program to count the total number of digits in a number using a while loop.
+# For example, the number is 75869, so the output should be 5. len()
+
+def digits(number):
+    for i in range(len(number)):
+        print((i))
+
+
+digits(75869)
+
+
+number = 75869
+for i in range(len(number)):
+    print(i)
