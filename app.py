87384a17cc51430b8208b6a30c0e60ba888bf9aa
Jose Armas <armasjose95@gmail.com>
2023-09-16 15:27:20 -0500

    printing patterns using for loops

--- app.py
+++ app.py
@@ -6757,3 +6757,66 @@ loops(5)
 # 3 2 1
 # 2 1
 # 1
+
+
+
+#Print list in reverse order using a loop
+list1 = [10, 20, 30, 40, 50]
+print(list1)
+list1.reverse()
+print(list1)
+
+list1 = [10, 20, 30, 40, 50]
+holder = []
+for i in list1:
+    holder = [i] + holder
+print(holder)
+
+
+# = [50, 40, 30, 20, 10]
+
+
+list1 = [10, 20, 30, 40, 50]
+# reverse list
+new_list = reversed(list1)
+# iterate reversed list
+for item in new_list:
+    print(item)
+    
+#50
+#40
+#30
+#20
+#10    
+
+
+
+#Display numbers from -10 to -1 using for loop
+for i in range(1,11,1):
+    print(i)
+    
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10    
+    
+    
+for i in range(-10,0,1):
+    print(i)
+    
+#-10
+#-9
+#-8
+#-7
+#-6
+#-5
+#-4
+#-3
+#-2
+#-1
