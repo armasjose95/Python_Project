6d260a724e0c7f54a2343d4432741af999f21119
Jose Armas <armasjose95@gmail.com>
2023-09-18 22:34:42 -0500

    adding break and else statments to my function

--- app.py
+++ app.py
@@ -6830,3 +6830,35 @@ for i in range(-10,0,1):
 
 for i in range(5):
     print(i)
+else:
+    print('Done!')
+    
+#1
+#2
+#3
+#4
+#Done!
+
+
+
+#Write a program to display all prime numbers within a range
+
+
+    start = 25
+    end = 50
+    for num in range(n,end + 1):
+        if num > 1:
+            for i in range(2,num):
+                if num % i == 0:
+                    break
+            else:
+                print(num)
+            
+     
+            
+#29
+#31
+#37
+#41
+#43
+#47
